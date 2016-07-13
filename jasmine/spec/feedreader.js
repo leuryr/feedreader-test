29b73e571bcacf6631b14a0e2bea48869ff38135
Leury Rodriguez <leuryr@yahoo.com>
2016-07-13 12:18:09 -0400

    Add test for defined URL, name, in allFeeds. Add test for menu hidden by default.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -32,27 +32,43 @@ $(function() {
          * and that the URL is not empty.
          */
 
+         it('have URL fields that are not empty', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            });
+         });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('have name fields that are not empty', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            });
+         });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function(){
+        var body = $('body');
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+         it('is hidden by default', function() {
+            expect(body.hasClass("menu-hidden")).toBe(true);
+         });
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
